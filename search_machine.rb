fb9f9e92051980355e8821c13d3b47cf04841ed6
Andrew M <beeengine@gmail.com>
2019-08-05 20:39:31 +0300

    code refactor

--- search_machine.rb
+++ search_machine.rb
@@ -3,21 +3,25 @@ require './parser_machine'
 
 class SearchMachine
   def initialize(data_file)
-    @data = JSON.parse(File.read(data_file))
-    @parsed = @data.map{ |lang| "#{lang['Name']} #{lang['Type']} #{lang['Designed by']}"} # all fields to one for scanning
+    @parsed = JSON.parse(File.read(data_file)).map do |lang| 
+      { 
+        search_string: lang.values.join(' '), #all fields to one for scanning
+        data: lang
+      }
+    end
   end
 
   def find(query)
     pm = ParserMachine.new(query)
     results = []
-    @parsed.each_with_index do |lang, index|
-      unless pm.regexes[:negative] && pm.regexes[:negative].match?(lang)
-        if pm.regexes[:exact_match].match?(lang)
-          results << @data[index].merge(relevance: 0) # adding language with highest relevance level to results
-        elsif pm.regexes[:any_order].match?(lang)
-          results << @data[index].merge(relevance: 1) # adding language with mid relevance level
-        elsif pm.regexes[:any_word].match?(lang)
-          results << @data[index].merge(relevance: 2) # adding language with low relevance level
+    @parsed.each do |lang|
+      unless pm.regexes[:negative] && pm.regexes[:negative].match?(lang[:search_string])
+        if pm.regexes[:exact_match].match?(lang[:search_string])
+          results << lang[:data].merge(relevance: 0) # adding language with highest relevance level to results
+        elsif pm.regexes[:any_order].match?(lang[:search_string])
+          results << lang[:data].merge(relevance: 1) # adding language with mid relevance level
+        elsif pm.regexes[:any_word].match?(lang[:search_string])
+          results << lang[:data].merge(relevance: 2) # adding language with low relevance level
         end
       end
     end
